df7e1153d4789d38b6a695e5134e8c2f08cd1a60
Andrew Wong <wong.andrew.p@gmail.com>
2017-05-01 12:29:48 -0400

    First commit

---
+++ src/main/java/wong/andrew/rotatearray/Main.java
@@ -0,0 +1,18 @@
+package wong.andrew.rotatearray;
+import java.util.*;
+
+/**
+ * Created by andrewwong on 5/1/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<Integer>();
+        for (int i = 1; i <7; i++){
+            nums.add(i);
+        }
+        System.out.println(nums);
+    }
+    public List rotate(List<Integer> nums, int k){
+        
+    }
+}
